447752c03e1cf0014b90a0e55910b974c2fd7117
Ismail Zare <zarehesmaiel@gmail.com>
2022-01-21 20:10:23 +0330

    Fixed missing column

--- database/migrations/2021_12_01_000009_create_koomeh_conditions_table.php
+++ database/migrations/2021_12_01_000009_create_koomeh_conditions_table.php
@@ -16,6 +16,7 @@ class CreateKoomehConditionsTable extends Migration
         Schema::create('koomeh_conditions', function (Blueprint $table) {
             $table->id();
             $table->json('name');  
+            $table->json('help')->nullable();  
             $table->foreignId('group_id')->constrained('koomeh_condition_groups');  
             $table->softDeletes(); 
             $table->timestamps(); 
